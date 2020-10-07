67f0cfcc350cf64dba6ca3d57485bdef79891c51
zhangzhimin17 <tszzmin@hotmail.com>
2020-10-07 14:33:33 +0800

    资源路由方式实现新闻的管理

--- application/admin/controller/News.php
+++ application/admin/controller/News.php
@@ -8,41 +8,77 @@ class News extends Controller
 {
     //显示所有新闻
     public function index(){
-        echo 'index';
+        //获取数据
+        $data = Db::table('news')->select();
+
+        //模板变量赋值
+        $this->assign('data', $data);
+
+        //渲染模板
+        return $this->fetch();
     }
 
     /**
      * 显示添加新闻信息
      */
     public function create(){
-        echo 'add';
+        return $this->fetch();
     }
+
     /**
      * 添加新闻信息到数据库
      */
     public function save(){
+        $data = array();
+        $data['title'] = $_POST['title'];
+        $data['content'] = $_POST['content'];
+
+        Db::table('news')->insert($data);
+
+        $this->success('添加成功', '/news');
     }
+
+
     /**
      * 显示编辑新闻信息模板
      */
     public function edit($id){
-        echo 'edit';
+        //获取指定id的新闻
+        $new = Db::table('news')->find($id);
+
+        //模板变量赋值
+        $this->assign('data', $new);
+
+        //渲染模板
+        return $this->fetch();
     }
+
     /**
      * 更新新闻信息
      */
-    public function update(){
+    public function update($id){
+        $data = array();
+        $data['title'] = input('title');
+        $data['content'] = input('content');
+
+        Db::table('news')->where('id', $id)->update($data);
+
+        echo 1;
     }
+
     /**
      * 查看新闻详细信息
      */
     public function read($id){
-        echo 'read';
+        echo $id;
     }
+
     /**
      * 删除新闻
      */
     public function delete($id){
+        Db::table('news')->delete($id);
 
+        echo '1';
     }
 }
